d242307b2636f6b2961a8b8729d064eeefb6d72f
tlei <tlei@haverford.edu>
2025-06-03 15:03:16 -0400

    packages

---
+++ ros2_ws/src/server_client/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+package_name = 'server_client'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='tlei',
+    maintainer_email='tlei@haverford.edu',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'server:server_client.server:main',
+            'client:server_client.client:main'
+        ],
+    },
+)
